aec08b3f8e55fcfd288a838e7b5dd07249d0555c
Timothy Walker <twalker@bb136-10.adit.mines.edu>
2017-06-08 11:32:18 -0600

    Icons change color now too!

--- js/map.js
+++ js/map.js
@@ -10,19 +10,28 @@ var map = (function() {
    						"weight": 5,
 							"opacity": 0.65
 			},
-		onEachFeature: popupWindow
+		onEachFeature: popupWindow,
+		pointToLayer: function(feature, latlng) {
+			return L.marker(latlng, {icon: orangeIcon});
+		},
 		}).on('layeradd', function(e) { lastLayer = e.layer; });
 	var selectedLayer = L.geoJson([], {
 		style: 	{	"color": "#ffff00",
    						"weight": 5,
 							"opacity": 0.65
-			},
+		},
+		pointToLayer: function(feature, latlng) {
+			return L.marker(latlng, {icon: greenIcon});
+		},
 		});
 	var editingLayers = L.geoJson([], {
 		style: 	{	"weight": 5,
 							"opacity": 0.65
-			},
-		onEachFeature: popupWindow
+		},
+		onEachFeature: popupWindow,
+		pointToLayer: function(feature, latlng) {
+			return L.marker(latlng, {icon: blueIcon});
+		},
 		}).on('layeradd', function(e) { lastLayer = e.layer; });
 	var lastFeature = null;
 	var lastLayer = null;
@@ -68,6 +77,8 @@ var map = (function() {
 			infoWindow.style.display = "";
 			iw_form.innerHTML = "<div class='row'><div class='form-group'><div class='col-md-4 col-sm-12'><span for='osm_id' class='label label-primary'>OSM ID</span></div><div class='col-md-8 col-sm-12'><input type='text' name='osm_id' id='osm_id' class='form-control' value="
 				+ feature.id + " disabled></div></div>";
+			properties = [];
+
 			for (var key in feature.properties) {
 				if (feature.properties.hasOwnProperty(key)) {
 					properties.push(key);
@@ -86,6 +97,7 @@ var map = (function() {
 
 	function submitPointQuery() {
 		// queries a single s2cell
+
 		var date = document.getElementById('calendar').value;
 		timestamp = new Date(date);
 		timestamp.setHours(document.getElementById('ts_hours').value);
@@ -169,6 +181,7 @@ var map = (function() {
 	}
 
 	function addFeature(feature) {
+		// Opens properties panel
 		lastFeature = feature;
 		infoWindow.style.display = "";
 		iw_form.innerHTML = "";
@@ -180,6 +193,7 @@ var map = (function() {
 	}
 
 	function addProperty() {
+		// adds new value to properties panel for more custom features
 		var key = prompt("New property:", "key");
 		if (properties.indexOf(key) > -1) return;	// if key already in properties, don't add
 		properties.push(key);
@@ -193,8 +207,12 @@ var map = (function() {
 	
 	function deleteFeature() {
 		if (lastFeature.hasOwnProperty('id')) {
-			// send feature id to thrift for deletion TODO: FINISH
+			// send feature id to thrift for deletion TODO: TEST
 			console.log("to thrift: delete " + lastFeature.id);
+			var transport = new Thrift.TXHRTransport("http://localhost:8000/service");
+			var protocol = new Thrift.TJSONProtocol(transport);
+			var client = new GeolocationServiceClient(protocol);
+			client.deleteFeature(lastFeature.id);
 		}
 
 		// hide panel and remove feature from all layers
@@ -221,13 +239,19 @@ var map = (function() {
 		try {
 			// check if valid geoJSON
 			geojson.addData(newFeature);
-			if (!lastFeature.hasOwnProperty('id')) {
-				// send new feature to thrift for processing TODO: FINISH
-				console.log("to thrift: add " + JSON.stringify(newFeature));
-			} else if (JSON.stringify(newFeatureProperties) != JSON.stringify(lastFeature.properties)) {
-				// send edited feature to thrift for processing TODO: FINISH
-				console.log("to thrift: update " + lastFeature.id + " to " + JSON.stringify(newFeature));
 
+			// set up thrift
+			var transport = new Thrift.TXHRTransport("http://localhost:8000/service");
+			var protocol = new Thrift.TJSONProtocol(transport);
+			var client = new GeolocationServiceClient(protocol);
+			if (!lastFeature.hasOwnProperty('id')) {
+				// send new feature to thrift for processing TODO: TEST
+				console.log("to thrift: add " + JSON.stringify(newFeature));
+				client.updateFeature(-1, JSON.stringify(newFeature));							
+			} else if (JSON.stringify(newFeatureProperties) != JSON.stringify(lastFeature.properties)) {
+				// send edited feature to thrift for processing TODO: TEST
+				console.log("to thrift: update " + lastFeature.id + " to " + JSON.stringify(newFeature));
+				client.updateFeature(lastFeature.id, JSON.stringify(newFeature));				
 			}
 		} catch(e) {
 			window.alert("Invalid GeoJSON");
@@ -235,6 +259,7 @@ var map = (function() {
 	}
 
 	function cancelAdd() {
+		// closes the window
 		selectedLayer.clearLayers();
 		cancel('info_window');
 	}
