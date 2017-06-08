e666fb6ec51a525db1452e1aa655dc90ea313788
Timothy Walker <twalker@bb136-10.adit.mines.edu>
2017-06-08 13:19:27 -0600

    There's a bug with submit

--- js/map.js
+++ js/map.js
@@ -75,41 +75,6 @@ var map = (function() {
 		editingLayers.addTo(map);
 	}
 
-	function popupWindow(feature, layer) {
-		layer.on('click', function (e) {
-			// dehighlight last feature
-			if (!(lastFeature === null)) {
-				if (lastFeature.hasOwnProperty('id')) { geojson.addData(lastFeature); }
-				else { editingLayers.addData(lastFeature); }
-			}
-
-			lastFeature = feature;
-			lastLayer = layer;
-
-			// create info panel
-			infoWindow.style.display = "";
-			iw_form.innerHTML = "<div class='row'><div class='form-group'><div class='col-md-4 col-sm-12'><span for='osm_id' class='label label-primary'>OSM ID</span></div><div class='col-md-8 col-sm-12'><input type='text' name='osm_id' id='osm_id' class='form-control' value="
-				+ feature.id + " disabled></div></div>";
-			properties = [];
-
-			for (var key in feature.properties) {
-				if (feature.properties.hasOwnProperty(key)) {
-					properties.push(key);
-					iw_form.innerHTML+="<div class='row'><div class='form-group'><div class='col-md-4 col-sm-12'><span for='"
-						+ key +"' class='label label-info'>" + key 
-						+ "</span></div><div class='col-md-8 col-sm-12'><input type='text' class='form-control' name='"
-						+ key + "' id='" + key + "' value='" + feature.properties[key] + "'></div></div>";
-				}
-			}
-
-			// highlight this feature
-			selectedLayer.clearLayers();
-			editingLayers.removeLayer(layer);
-			geojson.removeLayer(layer);
-			selectedLayer.addData(feature);
-		});
-	}
-
 	function submitPointQuery() {
 		// queries a single s2cell
 
@@ -195,6 +160,41 @@ var map = (function() {
 		}
 	}
 
+	function popupWindow(feature, layer) {
+		layer.on('click', function (e) {
+			// dehighlight last feature
+			if (!(lastFeature === null)) {
+				if (lastFeature.hasOwnProperty('id')) { geojson.addData(lastFeature); }
+				else { editingLayers.addData(lastFeature); }
+			}
+
+			lastFeature = feature;
+			lastLayer = layer;
+
+			// create info panel
+			infoWindow.style.display = "";
+			iw_form.innerHTML = "<div class='row'><div class='form-group'><div class='col-md-4 col-sm-12'><span for='osm_id' class='label label-primary'>OSM ID</span></div><div class='col-md-8 col-sm-12'><input type='text' name='osm_id' id='osm_id' class='form-control' value="
+				+ feature.id + " disabled></div></div>";
+			properties = [];
+
+			for (var key in feature.properties) {
+				if (feature.properties.hasOwnProperty(key)) {
+					properties.push(key);
+					iw_form.innerHTML+="<div class='row'><div class='form-group'><div class='col-md-4 col-sm-12'><span for='"
+						+ key +"' class='label label-info'>" + key 
+						+ "</span></div><div class='col-md-8 col-sm-12'><input type='text' class='form-control' name='"
+						+ key + "' id='" + key + "' value='" + feature.properties[key] + "'></div></div>";
+				}
+			}
+
+			// highlight this feature
+			selectedLayer.clearLayers();
+			editingLayers.removeLayer(layer);
+			geojson.removeLayer(layer);
+			selectedLayer.addData(feature);
+		});
+	}
+
 	function addFeature(feature) {
 		// Opens properties panel
 		lastFeature = feature;
@@ -227,7 +227,7 @@ var map = (function() {
 			var transport = new Thrift.TXHRTransport("http://localhost:8000/service");
 			var protocol = new Thrift.TJSONProtocol(transport);
 			var client = new GeolocationServiceClient(protocol);
-			client.deleteFeature(lastFeature.id);
+//			client.deleteFeature(lastFeature.id);
 		}
 
 		// hide panel and remove feature from all layers
@@ -239,8 +239,11 @@ var map = (function() {
 	}
 
 	function editFeature() {
-		// hide panel and remove feature from edit/select layers
-		cancelAdd();
+		// hide panel and remove feature from layers
+		selectedLayer.clearLayers();
+		editingLayers.removeLayer(lastLayer);
+		geojson.removeLayer(lastLayer);
+		cancel('info_window');
 
 		// read all boxes in to new geoJSON
 		var newFeatureProperties = {};
@@ -248,27 +251,39 @@ var map = (function() {
 			newFeatureProperties[iw_form.elements[i].id] = iw_form.elements[i].value;
 		}
 
-		var newFeature = lastFeature;
-		newFeature.properties = newFeatureProperties
 		try {
-			// check if valid geoJSON
-			geojson.addData(newFeature);
-
 			// set up thrift
 			var transport = new Thrift.TXHRTransport("http://localhost:8000/service");
 			var protocol = new Thrift.TJSONProtocol(transport);
 			var client = new GeolocationServiceClient(protocol);
 			if (!lastFeature.hasOwnProperty('id')) {
-				// send new feature to thrift for processing TODO: TEST
+				// check if valid geoJSON							
+				var newFeature = lastFeature;
+				newFeature.properties = newFeatureProperties;
+				geojson.addData(newFeature);
+
+				// send new feature to thrift for processing TODO: TEST							
 				console.log("to thrift: add " + JSON.stringify(newFeature));
-				client.updateFeature(-1, JSON.stringify(newFeature));							
+//				client.updateFeature(-1, JSON.stringify(newFeature));
 			} else if (JSON.stringify(newFeatureProperties) != JSON.stringify(lastFeature.properties)) {
+				// check if valid geoJSON
+				var newFeature = lastFeature;
+				newFeature.properties = newFeatureProperties;
+				geojson.addData(newFeature);
+
 				// send edited feature to thrift for processing TODO: TEST
 				console.log("to thrift: update " + lastFeature.id + " to " + JSON.stringify(newFeature));
-				client.updateFeature(lastFeature.id, JSON.stringify(newFeature));				
+//				client.updateFeature(lastFeature.id, JSON.stringify(newFeature));				
+			} else {
+				// add back to geojson layer
+				geojson.addData(lastFeature);
 			}
 		} catch(e) {
+			console.log(e);
 			window.alert("Invalid GeoJSON");
+			// failed: move layer back to previous state
+			if (lastFeature.hasOwnProperty('id')) { geojson.addData(lastFeature); }
+			else { editingLayers.addData(lastFeature); }
 		}
 	}
 
